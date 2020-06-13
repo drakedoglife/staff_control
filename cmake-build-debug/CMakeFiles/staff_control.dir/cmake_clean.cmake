b1de11bb122c983b23b5a14bd43e06844c2a50ce
drakedoglife <drakedoglife@outlook.com>
2020-06-13 13:56:32 +0800

    start

---
+++ cmake-build-debug/CMakeFiles/staff_control.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/staff_control.dir/main.cpp.obj"
+  "libstaff_control.dll.a"
+  "staff_control.exe"
+  "staff_control.exe.manifest"
+  "staff_control.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/staff_control.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
